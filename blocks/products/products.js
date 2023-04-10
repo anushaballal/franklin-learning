2ebfa16f9916e3a8fa3450970163a63f6508c713
Anusha Ballal <anushaballal@gmail.com>
2023-04-10 17:36:18 +0530

    js fix

--- blocks/products/products.js
+++ blocks/products/products.js
@@ -5,7 +5,6 @@ async function getJSONData() {
     "https://day-5--franklinlearning--anushaballal.hlx.page/project-data.json?sheet=products"
   );
   const jsonData = await response.json();
-
   return jsonData;
 }
 
@@ -16,18 +15,19 @@ export default async function decorate(block) {
   table.append(thead, tbody);
   const productData = await getJSONData();
   const products = productData.data;
-  console.log(products);
-  /* for (let product in productData.data) {
-    console.log(productData.data);
-    const row = document.createElement("tr");
+  for (let i = 0; i < products.length; i += 1) {
+    console.log(products[i]);
+    console.log(products[i].products);
+    console.log(products[i][products]);
+    /* const row = document.createElement("tr");
     tbody.append(row);
     const cell1 = document.createElement("td");
-    cell1.innerHTML = product.products;
+    cell1.innerHTML = products[i].products;
     row.append(cell1);
     const cell2 = document.createElement("td");
     cell2.innerHTML = product[release - year];
-    row.append(cell2);
-  } */
+    row.append(cell2); */
+  }
   block.innerHTML = "";
   block.append(table);
 }
