96a4b0a7a0d298e906acd5e9fec157c5490985f7
Anusha Ballal <anushaballal@gmail.com>
2023-04-10 17:39:42 +0530

    js fix

--- blocks/products/products.js
+++ blocks/products/products.js
@@ -16,18 +16,16 @@ export default async function decorate(block) {
   const productData = await getJSONData();
   const products = productData.data;
   for (let i = 0; i < products.length; i += 1) {
-    console.log(products[i]);
-    console.log(products[i].products);
-    console.log(products[i][products]);
-    /* const row = document.createElement("tr");
+    const row = document.createElement("tr");
     tbody.append(row);
     const cell1 = document.createElement("td");
     cell1.innerHTML = products[i].products;
     row.append(cell1);
     const cell2 = document.createElement("td");
-    cell2.innerHTML = product[release - year];
-    row.append(cell2); */
+    cell2.innerHTML = products[i].releaseYear;
+    row.append(cell2);
   }
   block.innerHTML = "";
   block.append(table);
+  console.log("done");
 }
