5ce22c424b0f3fccf5d9ac3d2677b0730b67104d
Anusha Ballal <anushaballal@gmail.com>
2023-04-13 14:21:51 +0530

    lint issues fix

--- blocks/embed/embed.js
+++ blocks/embed/embed.js
@@ -16,9 +16,7 @@ const loadScript = (url, callback, type) => {
   return script;
 };
 
-const getDefaultEmbed = (
-  url
-) => `<div style="left: 0; width: 100%; height: 0; position: relative; padding-bottom: 56.25%;">
+const getDefaultEmbed = (url) => `<div style="left: 0; width: 100%; height: 0; position: relative; padding-bottom: 56.25%;">
       <iframe src="${url.href}" style="border: 0; top: 0; left: 0; width: 100%; height: 100%; position: absolute;" allowfullscreen=""
         scrolling="no" allow="encrypted-media" title="Content from ${url.hostname}" loading="lazy">
       </iframe>
@@ -33,9 +31,7 @@ const embedYoutube = (url, autoplay) => {
     [, vid] = url.pathname.split('/');
   }
   const embedHTML = `<div style="left: 0; width: 100%; height: 0; position: relative; padding-bottom: 56.25%;">
-        <iframe src="https://www.youtube.com${
-          vid ? `/embed/${vid}?rel=0&v=${vid}${suffix}` : embed
-        }" style="border: 0; top: 0; left: 0; width: 100%; height: 100%; position: absolute;" 
+        <iframe src="https://www.youtube.com${vid ? `/embed/${vid}?rel=0&v=${vid}${suffix}` : embed}" style="border: 0; top: 0; left: 0; width: 100%; height: 100%; position: absolute;" 
         allow="autoplay; fullscreen; picture-in-picture; encrypted-media; accelerometer; gyroscope; picture-in-picture" allowfullscreen="" scrolling="no" title="Content from Youtube" loading="lazy"></iframe>
       </div>`;
   return embedHTML;
@@ -99,8 +95,7 @@ export default function decorate(block) {
   if (placeholder) {
     const wrapper = document.createElement('div');
     wrapper.className = 'embed-placeholder';
-    wrapper.innerHTML =
-      '<div class="embed-placeholder-play"><button title="Play"></button></div>';
+    wrapper.innerHTML = '<div class="embed-placeholder-play"><button title="Play"></button></div>';
     wrapper.prepend(placeholder);
     wrapper.addEventListener('click', () => {
       loadEmbed(block, link, true);
